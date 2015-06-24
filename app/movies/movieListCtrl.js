a9155191c96e86c27be416b3b5b6773c25995453
Santhosh Kumar Bala Krishnan <sbalakrishnan@vmware.com>
2015-06-24 11:58:25 -0700

    Added movie list view

---
+++ app/movies/movieListCtrl.js
@@ -0,0 +1,45 @@
+(function() {
+	
+	'use strict';
+	
+	angular.module("movieHunter")
+		.controller("MovieListCtrl",
+					[MovieListCtrl]);
+					
+	function MovieListCtrl() {
+		var data = this;
+		
+		data.movies = [
+            {
+                movieId: 1,
+                title: 'The Fellowship of the Ring',
+                director: 'Jackson',
+                releaseDate: '2001-12-19',
+                imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Fellowship-Of-The-Ring_small.jpg',
+                mpaa: 'pg-13'
+            },
+            {
+                movieId: 2,
+                title: 'The Two Towers',
+                director: 'Jackson',
+                releaseDate: '2002-12-18',
+                imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Two-Towers_small.jpg',
+                mpaa: 'pg-13'
+            },
+            {
+                movieId: 3,
+                title: 'The Point',
+                director: 'Wolf',
+                releaseDate: '1971-02-02',
+                imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Two-Towers_small.jpg',
+                mpaa: 'nr'
+            }];
+        data.title = "Search by Movie Title";
+        data.showImage = false;
+
+        data.toggleImage = function () {
+            data.showImage = !data.showImage;
+        };
+	}
+	
+}());
